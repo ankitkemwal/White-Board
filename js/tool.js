a8a414edf71abdc6139f261c8656c3b9e334d95c
ankitkemwal <86829818+ankitkemwal@users.noreply.github.com>
2022-11-16 20:57:18 +0530

    Update tool.js

--- js/tool.js
+++ js/tool.js
@@ -3,21 +3,23 @@
 
 // *********************************Basic Setup
 const board = document.querySelector(".board");
-board.height = window.innerHeight;
+board.height = window.innerHeight;  //screen dimn
 board.width = window.innerWidth;
 
-// canvasRenderingContext2d=> tool or ctx
-const ctx = board.getContext("2d");
+// canvasRenderingContext2d=> tool or ctx 
+const ctx = board.getContext("2d"); 
+// built-in HTML object, with properties and methods for drawing on the canvas
 
-// ctx.fillStyle = "green"
+// ctx.fillStyle = "yellow"
 // ctx.fillRect(0, 0, board.width, board.height);
+// Start at the upper-left corner (0,0) and draw a 150x75 pixels rectangle.
 
-ctx.strokeStyle = "blue";
+ctx.strokeStyle = "blue"; 
 ctx.imageSmoothingEnabled = true;
 ctx.lineJoin = "round"; // rounded shape will come at the intersection of two lines
 ctx.lineCap = "round"; // rounded cap will come at the endpoints of a line
 ctx.imageSmoothingQuality = "high";
-ctx.lineWidth = 3;
+ctx.lineWidth = 3;  //sets line width
 
 // ************************Change Size**************************//
 function sizeChange(value) {
